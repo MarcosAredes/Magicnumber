4eee62983e92d4f0b2bb244396e00c61069edde8
Marcos <106114852+MarcosAredes@users.noreply.github.com>
2025-09-05 22:11:28 -0300

    Update app.py
    New

--- app.py
+++ app.py
@@ -148,20 +148,12 @@ with aba1:
 
 
 
-  if st.session_state.acoes:
-    st.subheader("❌ Remover Ação")
+    if st.session_state.acoes:
+        st.subheader("❌ Remover Ação")
+        remover = st.selectbox("Selecione a ação para remover:", [r["NOME"] for r in st.session_state.acoes])
 
-    with st.form("form_remover"):
-        remover = st.selectbox(
-            "Selecione a ação para remover:",
-            [r["NOME"] for r in st.session_state.acoes]
-        )
-        confirmar_remover = st.form_submit_button("Remover Ação")
-
-        if confirmar_remover and remover:
-            st.session_state.acoes = [
-                r for r in st.session_state.acoes if r["NOME"] != remover
-            ]
+        if st.button("Remover Ação"):
+            st.session_state.acoes = [r for r in st.session_state.acoes if r["NOME"] != remover]
             st.warning(f"Ação {remover} removida!")
 
 
@@ -361,15 +353,21 @@ with aba4:
             })
             st.success(f"Moeda {nome} adicionada!")
 
+    if st.session_state.acoes:
+        st.subheader("❌ Remover Ação")
 
-    # Remover Moeda Estrangeira
-    if st.session_state.extern:
-        st.subheader("❌ Remover Moeda")
-        remover = st.selectbox("Selecione a moeda para remover:", [m["Moeda"] for m in st.session_state.extern])
-        if st.button("Remover Moeda"):
-            st.session_state.extern = [m for m in st.session_state.extern if m["Moeda"] != remover]
-            st.warning(f"Moeda {remover} removida!")
+        with st.form("form_remover"):
+            remover = st.selectbox(
+                "Selecione a ação para remover:",
+                [r["NOME"] for r in st.session_state.acoes]
+            )
+            confirmar_remover = st.form_submit_button("Remover Ação")
 
+            if confirmar_remover and remover:
+                st.session_state.acoes = [
+                    r for r in st.session_state.acoes if r["NOME"] != remover
+                ]
+                st.warning(f"Ação {remover} removida!")
 
     # Mostrar tabela e gráficos
     if st.session_state.extern:
@@ -436,7 +434,5 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-
-
 
 
